b3903779ebad612063c8b2d6d7d326204dcb7369
Andrea Sanguineti <andrea.sanguineti@mail.polimi.it>
2023-10-21 18:06:01 +0200

    Grounded with Raycast and coyote time

--- Don't stop me now!/Assets/Scripts/PlayerMovement.cs
+++ Don't stop me now!/Assets/Scripts/PlayerMovement.cs
@@ -11,6 +11,7 @@ public class PlayerMovement : MonoBehaviour
     public float speed;
     public float bounceIntensity;
     public float maxAngle;
+    public float coyoteTime = 0.25f;
     
     private Rigidbody2D _rb;
     private Collider2D _collider;
@@ -19,20 +20,21 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 _size;
     private bool _shouldJump;
     private bool _isGrounded;
+    private bool _inCojoneTime;
 
     private void OnEnable()
     {
-        _inputActions = new PlayerInputAction();
+        /*_inputActions = new PlayerInputAction();
         _inputActions.Gameplay.Enable();
         _inputActions.Gameplay.Jump.started += JumpStarted;
-        _inputActions.Gameplay.Start.started += StartVelocity;
+        _inputActions.Gameplay.Start.started += StartVelocity;*/
     }
     
     private void OnDisable()
     {
-        _inputActions.Gameplay.Disable();
+        /*_inputActions.Gameplay.Disable();
         _inputActions.Gameplay.Jump.started -= JumpStarted;
-        _inputActions = null;
+        _inputActions = null;*/
     }
 
     void Start()
@@ -46,32 +48,49 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         _sprite.color = _isGrounded ? Color.green : Color.red;
+        //Debug.Log(_inCojoneTime);
+        bool conditionCojone = _inCojoneTime && _rb.velocity.y < 0;
+        if(Input.GetKeyDown(KeyCode.Space) && (_isGrounded||conditionCojone))
+            _shouldJump = true;
     }
     void FixedUpdate()
     {
-        Vector2 pos = transform.position ;
-        RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.down);
-        if (hit.collider is not null)
+        Vector2 pos = transform.position;
+        // max distance for the raycast to check the hit
+        float groundDistance = _size.y / 2 + 0.01f;
+        float raycastOffset = _size.x / 2 - 0.01f;
+        // raycast on the left edge
+        RaycastHit2D hit1 = Physics2D.Raycast(pos + Vector2.left*raycastOffset, Vector2.down, groundDistance);
+        // raycast on the right edge
+        RaycastHit2D hit2 = Physics2D.Raycast(pos + Vector2.right*raycastOffset, Vector2.down, groundDistance);
+        // grounded if at least one of the 2 raycasts hit
+        if (hit1.collider is not null || hit2.collider is not null)
+            _isGrounded = true;
+        else
         {
-            float distance = pos.y - hit.point.y;
-            _isGrounded = distance - _size.y/2 <= 0.1f;
+            _isGrounded = false;
+            // wait for coyote time before disabling jump
+            if (!_inCojoneTime)
+            {
+                StartCoroutine(CoyoneTimer());
+            }
         }
         
+        // keep y velocity to 0 while on ground
         if (_isGrounded)
         {
             float dirX = Mathf.Sign(_rb.velocity.x);
             _rb.velocity = new Vector2(speed * dirX,.0f);
-            //TODO put the buffer
-            if (_shouldJump)
-            {
-                _rb.AddForce(Vector2.up * bounceIntensity, ForceMode2D.Impulse);
-                _shouldJump = false;
-                _isGrounded = false;
-            }
         }
-        
+        // jump
+        if (_shouldJump)
+        {
+            _rb.AddForce(Vector2.up * bounceIntensity, ForceMode2D.Impulse);
+            _shouldJump = false;
+            _isGrounded = false;
+        }
     }
-    private void StartVelocity(InputAction.CallbackContext context)
+    /*private void StartVelocity(InputAction.CallbackContext context)
     {
         _rb.AddForce(speed*Vector2.right, ForceMode2D.Impulse);
         _inputActions.Gameplay.Start.started -= StartVelocity;
@@ -80,5 +99,12 @@ public class PlayerMovement : MonoBehaviour
     {
         if (_isGrounded)
             _shouldJump = true;
+    }*/
+    
+    private IEnumerator CoyoneTimer()
+    {
+        _inCojoneTime = true;
+        yield return new WaitForSeconds(coyoteTime);
+        _inCojoneTime = false;
     }
 }

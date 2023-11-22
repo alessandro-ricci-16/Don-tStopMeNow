177f1b7cccc24c18dc1c0e020156657dc538fffa
alessandro-ricci-16 <alessandro7.ricci@mail.polimi.it>
2023-11-22 13:27:50 +0100

    tutorial levels
    Folder creation for the tutorial levels and differentiation of single mechanics. Ale only wall jump and all tutorial levels implemented.

--- Don't stop me now!/Assets/Scripts/World elements/DeathZone.cs
+++ Don't stop me now!/Assets/Scripts/World elements/DeathZone.cs
@@ -4,8 +4,18 @@ public class DeathZone : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
+        CheckDeath(other);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        CheckDeath(collision.collider);
+    }
+
+    private void CheckDeath(Collider2D other)
+    {
         // if the player enters the death zone dies :)
-        if(other.CompareTag("Player"))
+        if (other.CompareTag("Player"))
             EventManager.TriggerEvent(EventNames.Death, other.transform.position);
     }
 }

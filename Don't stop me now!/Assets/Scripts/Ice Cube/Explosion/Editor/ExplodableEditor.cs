733559b1e10121b21f8f47577f7b0d25ad456acc
emanuelesantoro <emanuele1.santoro@mail.polimi.it>
2023-11-28 19:54:37 +0100

    add shatter for death

---
+++ Don't stop me now!/Assets/Scripts/Ice Cube/Explosion/Editor/ExplodableEditor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+/*
+[CustomEditor(typeof(Explodable))]
+public class ExplodableEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        Explodable myTarget = (Explodable)target;
+        myTarget.shatterType = (Explodable.ShatterType)EditorGUILayout.EnumPopup("Shatter Type", myTarget.shatterType);
+        myTarget.extraPoints = EditorGUILayout.IntField("Extra Points", myTarget.extraPoints);
+        myTarget.forceMagnitude = EditorGUILayout.FloatField("Force Magnitude", myTarget.forceMagnitude);
+        myTarget.subshatterSteps = EditorGUILayout.IntField("Subshatter Steps",myTarget.subshatterSteps);
+        if (myTarget.subshatterSteps > 1)
+        {
+            EditorGUILayout.HelpBox("Use subshatter steps with caution! Too many will break performance!!! Don't recommend more than 1", MessageType.Warning);
+        }
+
+        myTarget.fragmentLayer = EditorGUILayout.TextField("Fragment Layer", myTarget.fragmentLayer);
+        myTarget.sortingLayerName = EditorGUILayout.TextField("Sorting Layer", myTarget.sortingLayerName);
+        myTarget.orderInLayer = EditorGUILayout.IntField("Order In Layer", myTarget.orderInLayer);
+        
+        
+    }
+}*/

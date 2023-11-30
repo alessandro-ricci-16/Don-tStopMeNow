faf46d78265e2fd5ab73d0f2033edaa0455d999d
Sara Merengo <sara.merengo@mail.polimi.it>
2023-11-30 17:50:46 +0100

    add level number screen

--- Don't stop me now!/Assets/Scripts/Managers/UI Managers/LevelUIManager.cs
+++ Don't stop me now!/Assets/Scripts/Managers/UI Managers/LevelUIManager.cs
@@ -1,18 +1,34 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 public class LevelUIManager : MonoBehaviour
 {
     public GameObject pauseMenuCanvas;
-    public GameObject backgroundCanvas;
+    public Image backgroundImage;
+    public TextMeshProUGUI levelText;
+    
+    public float fadeTime = 1f;
+    
+    private Color backgroundColor;
 
     private void Start()
     {
         pauseMenuCanvas.SetActive(false);
-        backgroundCanvas.SetActive(false);
+
+        int levelIndex = SceneManager.GetActiveScene().buildIndex - 1;
+        levelText.text = "Level " + levelIndex;
+        
+        levelText.gameObject.SetActive(true);
+        backgroundColor = backgroundImage.color;
+        backgroundImage.gameObject.SetActive(true);
+        
+        StartCoroutine(FadeLevelText());
     }
 
     private void Update()
@@ -21,17 +37,39 @@ public class LevelUIManager : MonoBehaviour
             CallPause();
     }
 
+    private IEnumerator FadeLevelText()
+    {
+        float elapsedTime = 0;
+        Color textStartColor = levelText.color;
+        Color textEndColor = new Color(textStartColor.r, textStartColor.g, textStartColor.b, 0);
+        Color backgroundStartColor = backgroundImage.color;
+        Color backgroundEndColor = new Color(backgroundStartColor.r, backgroundStartColor.g, backgroundStartColor.b, 0);
+        
+        while (elapsedTime < fadeTime)
+        {
+            levelText.color = Color.Lerp(textStartColor, textEndColor, elapsedTime / fadeTime);
+            backgroundImage.color = Color.Lerp(backgroundStartColor, backgroundEndColor, elapsedTime / fadeTime);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        
+        levelText.gameObject.SetActive(false);
+        backgroundImage.gameObject.SetActive(false);
+        backgroundImage.color = backgroundColor;
+    }
+
     public void CallPause()
     {
         Time.timeScale = 0;
         pauseMenuCanvas.SetActive(true);
-        backgroundCanvas.SetActive(true);
+        backgroundImage.color = backgroundColor;
+        backgroundImage.gameObject.SetActive(true);
     }
 
     public void Resume()
     {
         pauseMenuCanvas.SetActive(false);
-        backgroundCanvas.SetActive(false);
+        backgroundImage.gameObject.SetActive(false);
         Time.timeScale = 1;
     }
 

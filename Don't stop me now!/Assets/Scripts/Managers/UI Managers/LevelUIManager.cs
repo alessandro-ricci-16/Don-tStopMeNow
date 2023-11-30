5c4d509a672f07293a963fe14690ef78e4fc1fcd
Sara Merengo <sara.merengo@mail.polimi.it>
2023-11-30 17:23:06 +0100

    fixes

--- Don't stop me now!/Assets/Scripts/Managers/UI Managers/LevelUIManager.cs
+++ Don't stop me now!/Assets/Scripts/Managers/UI Managers/LevelUIManager.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
 
-public class PauseMenuUIManager : MonoBehaviour
+public class LevelUIManager : MonoBehaviour
 {
     public GameObject pauseMenuCanvas;
     public GameObject backgroundCanvas;

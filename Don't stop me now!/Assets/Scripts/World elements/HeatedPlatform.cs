7b18918713eb5243e549d400ea44dcd4ba883604
emanuelesantoro <emanuele1.santoro@mail.polimi.it>
2024-01-10 13:50:24 +0100

    no rescale time while dashing heated and added checkpoint

--- Don't stop me now!/Assets/Scripts/World elements/HeatedPlatform.cs
+++ Don't stop me now!/Assets/Scripts/World elements/HeatedPlatform.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Ice_Cube.States;
 using ScriptableObjects;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -10,11 +11,11 @@ public class HeatedPlatform : MonoBehaviour
     // we have to create a function that when the player enters the heated platform, it will start a timer.
     // As long as it stays in contact with the platform the timer will increase,
     // when it reaches a max value the player will die
-    
+
     public HeatableSettings heatableSettings;
     private float _timer;
     private bool _isPlayerOn;
-    
+    private IceCubeStateManager _iceCubeStateManager;
     private bool _sentDeathEvent;
 
     private void Update()
@@ -22,12 +23,15 @@ public class HeatedPlatform : MonoBehaviour
         if (_isPlayerOn)
         {
             _timer += Time.deltaTime;
-
         }
         else if (_timer > 0)
         {
-            _timer -= heatableSettings.recoveryScale*Time.deltaTime;
-            _timer = Mathf.Clamp(_timer, 0, heatableSettings.maxTime);
+            if (_iceCubeStateManager is not null &&
+                _iceCubeStateManager.GetCurrentState().GetEnumState() != IceCubeStatesEnum.IsDashing)
+            {
+                _timer -= heatableSettings.recoveryScale * Time.deltaTime;
+                _timer = Mathf.Clamp(_timer, 0, heatableSettings.maxTime);
+            }
         }
     }
 
@@ -35,6 +39,8 @@ public class HeatedPlatform : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            if (_iceCubeStateManager is null)
+                _iceCubeStateManager = other.gameObject.GetComponent<IceCubeStateManager>();
             EventManager.TriggerEvent(EventNames.OnHeatedPlatform, _timer);
             _isPlayerOn = true;
         }
